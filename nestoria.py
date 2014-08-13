f7d52a5bc55a1294fc7398cc04b2ba06f49bbdb3
Stephen McCullough <me@swm.cc>
2014-08-13 10:10:30 +0100

    Added api_elements, _build_parameters, _get_results. URL no longer hardcoded

--- nestoria.py
+++ nestoria.py
@@ -1,16 +1,9 @@
-#!/usr/bin/env python
- 
 import urllib2
 import json
- 
-from collections import namedtuple
 
-# HARCODED SEARCH STRING - WILL REMOVE LATER 
-NESTORIA_API_URL = "http://api.nestoria.co.uk/api?country=uk&action=search_listings&place_name=glenavy&encoding=json&listing_type=buy&number_of_results='50'"
- 
-response = urllib2.urlopen(NESTORIA_API_URL)
-results = json.load(response) 
- 
+from collections import namedtuple
+from urllib import urlencode
+
 Property = namedtuple('Property', [
     "auction_date",  "bathroom_number", "bedroom_number", "car_spaces", 
     "commission", "construction_year", "datasource_name", "guid", "img_height", 
@@ -21,10 +14,43 @@ Property = namedtuple('Property', [
     "title", "updated_in_days", "updated_in_days_formatted",
 ])
  
-def get_properites():
-    properties = {}
+api_elements = [
+    'place_name', 'south_west', 'north_east', 'centre_point', 'radius',
+    'number_of_results', 'listing_type', 'property_type', 'price_max',' price_min',
+    'bedroom_max', 'bedroom_min', 'size_max', 'size_min', 'sort', 'keywords',
+    'keywords_exclude'
+]
  
+def _build_parameters(parameters={}):
+    api_parameters = {}
+    for element in [x for x in parameters if x in api_elements]:
+        # should validate the data passed in not just the element name
+        api_parameters[element] = parameters[element]
+
+    # these aren't really hardcoded so will change this
+    harcoded_parameters = {'action':'search_listings', 'country':'uk', 'encoding':'json'}
+
+    parameters = dict(api_parameters.items() + harcoded_parameters.items())
+    return urlencode(parameters)
+
+def _get_results(parameters={}):
+    api_parameters = _build_parameters(parameters)
+    
+    NESTORIA_API_URL = "http://api.nestoria.co.uk/api?" + api_parameters
+
+    response = urllib2.urlopen(NESTORIA_API_URL)
+    results = json.load(response)
+
+    return results
+
+def get_properites(parameters={}):
+    results = _get_results(parameters)
+    
+    properties = {}
     for result in results['response']['listings']:
         properties[result['guid']] = Property(**result)
- 
+
     return properties
+
+if __name__ == '__main__':
+    print get_properites({'place_name':'Ballymena'})
