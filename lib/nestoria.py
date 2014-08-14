ba123023de342fced1b7745765a8e450f125084b
Stephen McCullough <me@swm.cc>
2014-08-14 12:53:28 +0100

    #4 Just ignored it

--- lib/nestoria.py
+++ lib/nestoria.py
@@ -28,7 +28,7 @@ def _build_parameters(parameters={}):
         api_parameters[element] = parameters[element]
 
     # these aren't really hardcoded so will change this
-    harcoded_parameters = {'action':'search_listings', 'number_of_results':'50','country':'uk', 'encoding':'json'}
+    harcoded_parameters = {'action':'search_listings', 'number_of_results':'50', 'country':'uk', 'encoding':'json'}
 
     parameters = dict(api_parameters.items() + harcoded_parameters.items())
     return urlencode(parameters)
@@ -47,8 +47,12 @@ def search_listings(parameters={}):
     
     properties = {}
     results = _get_results(parameters)
+    print results
     if results['response']['application_response_code'].startswith('1'):
         for result in results['response']['listings']:
             properties[result['guid']] = Property(**result)
 
     return properties
+
+if __name__ == '__main__':
+    print search_listings({'place_name':'Glenavy'})
